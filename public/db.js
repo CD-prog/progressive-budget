46a1ce0171d7f597e183bf7f07e7c5dc35500788
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-09-15 10:51:07 -0500

    offline transactions db

---
+++ public/db.js
@@ -0,0 +1,61 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let db ;
+var request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+     db = event.target.result;
+   db.createObjectStore("pending",{autoIncrement: true})
+
+    };
+request.onsuccess = function(event) {
+     db = event.target.result;
+    if(navigator.onLine){
+        checkDatabase();
+    }
+};
+request.onerror = function(event) {
+    console.error("Error: " + event.target.errorCode);
+  };
+
+function saveRecord(record){
+    var transaction = db.transaction(['pending'],'readwrite')
+    var store = transaction.objectStore('pending')
+    store.add(record)
+}
+
+function checkDatabase() {
+    console.log('Database being checked')
+    var transaction = db.transaction(['pending'],'readwrite')
+    var store = transaction.objectStore('pending')
+    const allTransactions = store.getAll();
+
+    allTransactions.onsuccess = function(){
+        if(allTransactions.result.length > 0){
+            console.log('Transactions added...')
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(allTransactions.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                }
+              })
+                .then(response => {
+                    var transaction = db.transaction(['pending'], 'readwrite')
+                    var store = transaction.objectStore('pending')
+                    store.clear()
+                    return response.json();
+                })
+        }
+    }
+}
+
+window.addEventListener('online', checkDatabase);
+
+
